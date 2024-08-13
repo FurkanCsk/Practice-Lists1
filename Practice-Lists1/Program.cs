75d5eba7d92056bfd6f26754fbaa53087dfa55ee
FurkanCsk <furkancoskun99@hotmail.com>
2024-08-13 11:35:20 +0300

    Add project files.

---
+++ Practice-Lists1/Program.cs
@@ -0,0 +1,7 @@
+﻿List<string> visitors = new List<string>() { "Bülent Ersoy", "Ajda Pekkan", "Ebru Gündeş", "Hadise", "Hande Yener", "Tarkan", "Funda Arar", "Demet Akalın" };
+
+Console.WriteLine("** Visitors **\n");
+for (int i = 0; i < visitors.Count; i++)
+{
+    Console.WriteLine($"{i+1} - {visitors[i]}");
+}
