041f0cdeb19953b535180d1526f12d8828ad2f51
FurkanCsk <furkancoskun99@hotmail.com>
2024-08-13 11:40:00 +0300

    Fixed.

--- Practice-Lists1/Program.cs
+++ Practice-Lists1/Program.cs
@@ -1,6 +1,21 @@
-﻿List<string> visitors = new List<string>() { "Bülent Ersoy", "Ajda Pekkan", "Ebru Gündeş", "Hadise", "Hande Yener", "Tarkan", "Funda Arar", "Demet Akalın" };
+﻿
+// Create a list of visitors
+List<string> visitors = new List<string>() 
+{ 
+    "Bülent Ersoy", 
+    "Ajda Pekkan", 
+    "Ebru Gündeş", 
+    "Hadise", 
+    "Hande Yener", 
+    "Tarkan", 
+    "Funda Arar", 
+    "Demet Akalın" 
+};
 
+// Print the header
 Console.WriteLine("** Visitors **\n");
+
+//Loop through the  list and print each visitor
 for (int i = 0; i < visitors.Count; i++)
 {
     Console.WriteLine($"{i+1} - {visitors[i]}");
